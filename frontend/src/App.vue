0fd0248085e1f151a9d735ac579accf36786421c
sokoleg90 <sokoleg1990sokoleg@gmail.com>
2023-07-11 07:01:12 +0300

    start project

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <router-view></router-view>
+  <h1></h1>
+</template>
+
+<script>
+
+import {mapState} from "vuex";
+export default {
+  computed: {
+    ...mapState(['user'])
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/src/views/Surveys.vue
@@ -0,0 +1,10 @@
+<template>
+  <PageComponent title="Surveys">
+    Здесь будет ваш контент
+  </PageComponent>
+</template>
+
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+
+</script>
